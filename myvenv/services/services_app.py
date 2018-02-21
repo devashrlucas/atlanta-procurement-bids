de0ca76f9d26a8cf3000ff6139ad234c132f8894
devashrlucas <devashrlucas@gmail.com>
2018-02-21 15:40:27 -0500

    General commit.

--- myvenv/services/services_app.py
+++ myvenv/services/services_app.py
@@ -2,22 +2,22 @@
 #coding=utf-8
 from flask import Flask, jsonify
 
-#from services_scraper import keep_copy
+import services_scraper 
 
 app = Flask(__name__)
-'''
+
 @app.route('/')
 def post_solicitation():
-    return jsonify({'Solicitation': [item for item in keep_copy]})
+    return jsonify({'Solicitation': [item for item in services_scrapper.keep_copy]})
 
 
 if __name__ == '__main__':
     app.run(debug=True)
- '''
+'''
 @app.route("/")
 def hello():
     return "Hello World!"
  
 if __name__ == "__main__":
     app.run(debug=True)
-
+'''
