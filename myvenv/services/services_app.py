df0c5a2a2de4b6836157b1f39196adc7b6b09ecc
devashrlucas <devashrlucas@gmail.com>
2018-02-21 14:56:51 -0500

    Working on Flask.

--- myvenv/services/services_app.py
+++ myvenv/services/services_app.py
@@ -1,14 +1,25 @@
 #!flask/bin/python
+#coding=utf-8
 from flask import Flask, jsonify
 
 from services_scraper import keep_copy
 
 app = Flask(__name__)
-
-@app.route('/services/', methods=['POST'])
+'''
+@app.route('/')
 def post_solicitation():
     return jsonify({'Solicitation': [item for item in keep_copy]})
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+'''
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
--- myvenv/services/services_scraper.py
+++ myvenv/services/services_scraper.py
@@ -1,5 +1,5 @@
 
-
+#coding=utf-8
 from bs4 import BeautifulSoup, NavigableString
 import requests
 import simplejson as json
