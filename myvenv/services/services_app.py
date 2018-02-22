a0aabdeaacb28a1c301a7e0717cab1dcc0973ae4
devashrlucas <devashrlucas@gmail.com>
2018-02-21 20:49:08 -0500

    Added 404 error handler.

--- myvenv/services/services_app.py
+++ myvenv/services/services_app.py
@@ -1,17 +1,26 @@
 #!flask/bin/python
 #coding=utf-8
 from flask import Flask, jsonify
+from flask import make_response
+from flask import abort
 
 import services_scraper as ss
 
-ss.keep_copy = ss.get_info()
+keep_copy = ss.get_info()
 
 app = Flask(__name__)
 
 @app.route('/')
 def post_solicitation():
-    return jsonify({'Solicitation': [item for item in ss.keep_copy]})
+        if len(keep_copy) == 0:
+            abort(404)
+        else:
+            return jsonify({'Solicitation': keep_copy})
 
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'not found'}), 404)
 
 if __name__ == '__main__':
     app.run(debug=True)
