fbe3a418878ca76771f74e49dc3a5b69baf98f86
devashrlucas <devashrlucas@gmail.com>
2018-03-05 09:31:41 -0500

    Working on template.

--- myvenv/services/services_app.py
+++ myvenv/services/services_app.py
@@ -1,6 +1,6 @@
 #!flask/bin/python
 #coding=utf-8
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, render_template
 
 
 import services_scraper as ss
@@ -9,15 +9,25 @@ import services_scraper as ss
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    master_list = []
+
+@app.context_processor
+def for_template(): 
     #return jsonify('connected')
     for url in ss.urls:
+        master_list = []
         ss.get_info(url)
+        #keys = range(0, len(ss.full_set))
+        values = ss.full_set
         master_list.append(ss.full_set)
-        return jsonify(master_list)
-  
+        parent_dict = {key:value for key, value in enumerate(values)}
+        return (dict(parent_dict=master_list))
+        #parent_dict = dict(zip(keys, values))
+        #return jsonify(master_list)
+
+
+@app.route('/', methods=['GET'])
+def return_temp():
+    return render_template('services_template.html')
 
 @app.errorhandler(404)
 def not_found(error):
